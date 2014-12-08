809e55de320e956168ce4836307f62892f4ab03c
CyborgYL <gotolyh@gmail.com>
2014-12-08 07:25:19 -0500

    upload

--- Assets/Wireframe.js
+++
@@ -1,7 +0,0 @@
-// Attach this script to a camera, this will make it render in wireframe
-function OnPreRender() {
-    GL.wireframe = true;
-}
-function OnPostRender() {
-    GL.wireframe = false;
-}
